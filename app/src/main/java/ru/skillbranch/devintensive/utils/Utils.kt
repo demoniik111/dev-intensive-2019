2c89f51c343c8a879ed32dfad7be13f760c6094d
Dmitriy Zheltyy <dazheltyy@gmail.com>
2019-07-02 22:08:31 +0300

    hometask_2 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,29 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName:String?):Pair<String?,String?>{
+        //TODO FIX ME
+        if (fullName.isNullOrEmpty()) {
+            return Pair(null, null)
+        } else {
+            if (fullName != " ") {
+                val parts : List<String>? = fullName?.split(" ")
+
+                val firstName = parts?.getOrNull(0)
+                var lastName = parts?.getOrNull(1)
+
+                return Pair(firstName, lastName)
+            } else {
+                return Pair(null, null)
+            }
+        }
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        TODO("not implemented")
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        TODO("not implemented")
+    }
+}
